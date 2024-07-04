836bc6a0ce97654c29d92c3554c2675fa02b0a4e
Maltsev Ivan <vantos.maltsev@gmail.com>
2024-07-04 08:49:11 +0000

    Changed code format

--- airflow/dags/dag_3_4_7.py
+++ airflow/dags/dag_3_4_7.py
@@ -6,20 +6,20 @@ from datetime import datetime
 def my_func(hello, date, **context):
     print(hello)
     print(date)
-    print(context['task'].task_id)
+    print(context["task"].task_id)
 
 
 with DAG(
-    'dag_3_4_7',
-    schedule_interval='@daily',
+    "dag_3_4_7",
+    schedule_interval="@daily",
     start_date=datetime(2021, 1, 1),
     end_date=datetime(2021, 1, 10),
 ) as dag:
     python_task = PythonOperator(
-        task_id='python_task',
+        task_id="python_task",
         python_callable=my_func,
         op_kwargs={
-            'hello': "Hello World",
-            'date': "{{ execution_date }}",
+            "hello": "Hello World",
+            "date": "{{ execution_date }}",
         }
     )
