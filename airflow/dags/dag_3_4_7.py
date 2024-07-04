4618200b0e9a41c751724fdc85b742db05ce48c5
Maltsev Ivan <vantos.maltsev@gmail.com>
2024-07-04 08:40:06 +0000

    Added solution for lesson 3.4, step 7

---
+++ airflow/dags/dag_3_4_7.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+
+def my_func(hello, date, **context):
+    print(hello)
+    print(date)
+    print(context['task'].task_id)
+
+
+with DAG(
+    'dag_3_4_7',
+    schedule_interval='@daily',
+    start_date=datetime(2021, 1, 1),
+    end_date=datetime(2021, 1, 10),
+) as dag:
+    python_task = PythonOperator(
+        task_id='python_task',
+        python_callable=my_func,
+        op_kwargs={
+            'hello': "Hello World",
+            'date': "{{ execution_date }}",
+        }
+    )
