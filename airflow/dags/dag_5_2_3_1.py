5a30dc5e81d5a845416070545b36967e9bbddd2d
Maltsev Ivan <vantos.maltsev@gmail.com>
2024-07-15 16:18:22 +0000

    Added first code example for lesson 5.2, step 3

---
+++ airflow/dags/dag_5_2_3_1.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.utils.dates import days_ago
+
+
+dag = DAG(
+    "dag_5_2_3",
+    schedule_interval="@daily",
+    start_date=days_ago(1),
+)
+
+
+# Функция для условия выбора нужного task_id
+def branch_func(**kwargs):
+    xcom_value = int(kwargs["ti"].xcom_pull(task_ids="start_task"))
+    if xcom_value >= 5:
+        return "continue_task"
+    else:
+        return "stop_task"
+
+
+# Стартовый оператор который пуляет в xcom 10
+start_op = BashOperator(
+    task_id="start_task",
+    bash_command="echo 10",
+    dag=dag,
+)
+
+# Сам оператор условие
+branch_op = BranchPythonOperator(
+    task_id="branch_task",
+    provide_context=True,
+    python_callable=branch_func,
+    dag=dag,
+)
+
+continue_op = EmptyOperator(task_id="continue_task", dag=dag)
+stop_op = EmptyOperator(task_id="stop_task", dag=dag)
+
+start_op >> branch_op >> [continue_op, stop_op]
