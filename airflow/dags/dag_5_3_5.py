7a4bbdac8313231934af33fc0162223749f1cf05
Maltsev Ivan <vantos.maltsev@gmail.com>
2024-07-17 06:26:00 +0000

    Added solution for lesson 5.3, step 5

---
+++ airflow/dags/dag_5_3_5.py
@@ -0,0 +1,38 @@
+import random
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.hooks.base import BaseHook
+from airflow.models import BaseOperator
+from airflow.utils.dates import days_ago
+
+
+# Кастомный хук
+class CustomHook(BaseHook):
+
+    # Метод который генерирует случайное число
+    def random_number(self):
+        random.seed(10, version=2)
+        return random.randint(0, 10)
+
+
+# Кастомный оператор
+class CustomOperator(BaseOperator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hook = None  # Сюда мы будем передавать объект класса CustomHook
+
+    # Метод отправляет в Xcom некотрое значение
+    def execute(self, context):
+
+        self.hook = CustomHook()
+        return self.hook.random_number()
+
+
+dag = DAG(
+    'dag_5_3_5',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(1),
+)
+
+t1 = CustomOperator(task_id='task_1', dag=dag)
